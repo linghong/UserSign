434279051dd83d1dae00ba6f2a903750e2d39190
linghong <linghong_chen@hotmail.com>
2017-07-08 22:49:07 -0400

    created a JSON web token.

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,4 +1,11 @@
+const jwt =require('jwt-simple');
 const User =require('../models/user');
+const config =require ('../config');
+
+function tokenForUser(user){
+	const timestamp = new Date().getTime();
+	return jwt.encode({sub: user.id, iat: timestamp }, config.secret);
+}
 
 exports.signup = function(req, res, next){
 	const email =req.body.email;
@@ -25,11 +32,8 @@ exports.signup = function(req, res, next){
 		user.save(function(err){
 			if(err) {return next(err);}
 
-			res.json({success: true});
+			res.json({token: tokenForUser(user)});
 		});
 	});
-
-
-	//if a user with the given email doesn't exist
 	
 }
